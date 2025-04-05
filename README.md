25411bd29bd60198483aec4ddfb2d3eaca4106fe
queiroz <ferqueiroz2205@gmail.com>
2025-04-05 01:20:14 -0300

    Cadastro v1.0
    Pagina de login onde salva as informações em um array. Digitando uma string no campo vazio pode se pesquisar o nome, digitando um número, apaga o usuário daquele index.

---
+++ README.md
@@ -0,0 +1,27 @@
+# ATIVIDADE 03 - Front End (Hugo)
+
+Fazer um site de login, onde cadastra nome e idade, e armazenar em um array, com as opções de listar localizar o index da pessoa e excluí-la.
+
+## 🎥 Demonstração
+
+<img src="Imagens/pagina.png">
+
+## 🛠️ Construído com
+
+Ferramentas e tecnologias utilizadas nesse projeto
+
+- ✅ **HTML5** - Estruturação do site
+- ✅ **CSS3** - Estilização e responsividade
+- ✅ **JavaScript** - Utilização e interação do mapa
+- ✅ **Bootstrap** - Agilidade e design intuitivo
+
+
+## 📄 Licença
+
+Este projeto está sob a licença (queiroz) - veja o arquivo [LICENSE](https://github.com/ferqueiroz/AEP-2025/blob/main/LICENSE) para detalhes.
+
+## 📩 Contato  
+
+📧 **Email:** [ferqueiroz2205@gmail.com](mailto:ferqueiroz2205@gmail.com)<br>
+📷 **Instagram:** [@_ferqueiroz](https://instagram.com/_ferqueiroz)<br>
+👨‍💻 **GitHub:** [github.com/ferqueiroz](https://github.com/ferqueiroz)
---
+++ cadastro.js
@@ -0,0 +1,37 @@
+let usuario = [];
+
+function adicionar(){
+    let lNome = document.getElementById('edNome');
+    let lIdade = document.getElementById('edIdade');
+
+    usuario.push({'nome': lNome.value, 'idade': lIdade.value});
+    console.log(usuario);
+
+    lNome.value = "";
+    lIdade.value = "";
+}
+
+function verificar(){
+    let lPesquisa = document.getElementById('edVerificar');
+    let lIndex = usuario.findIndex(function(item){
+        return item.nome == lPesquisa.value;
+    });
+
+    alert(lIndex >= 0 ? 'Index:  '+ lIndex : 'Esse Usuário não existe.');
+
+    lPesquisa.value = '';
+}
+
+function remover(){
+    let lValor = document.getElementById('edVerificar').value == '' ? +document.getElementById('edVerificar').value : document.getElementById('edVerificar').value;
+
+    if (isNaN(lValor) || lValor == '')
+        return;
+
+    usuario.splice(lValor, lValor + 1);
+    console.log(usuario);
+
+    alert('Removido o Usuario do index:  ' + lValor);
+
+    document.getElementById('edVerificar').value = '';
+}
